43b9dfa1b7b919b9b93417477f916f53688bb7ff
Tadiwanashe Shangwa <tshxng@gmail.com>
2022-08-16 15:20:13 +0200

    First Update

--- src/App.js
+++ src/App.js
@@ -16,9 +16,14 @@ function App() {
           <img src={dp}  className="dp" alt="dp"/>
 
           {/* TODO:Add Follower Numbers */}
+          <h1>About Me</h1>
           <p>Hello there, my name is Tadiwa. Im a Computer Science student in High School, based in Zimbabwe 🇿🇼 passionate about Web Development and Software Engineering. Check out my socials links and projects down below ⬇️⬇️ </p>
+          <h1>My Socials</h1>
           <a className='socials' href='http://instagram.com/tadiwadev' > Instagram</a>
           <a className='socials' href='http://github.com/Tadiwr' >Github</a>
+          <a className='socials' href='https://twitter.com/tadiwrr' >Twitter</a>
+
+          <h1>My Projects</h1>
           <a className='socials' href='https://gituserquery.netlify.app/' >GitHub User Query</a>
       </div>
     </div>
