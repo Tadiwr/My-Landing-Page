c0f9176294bb585f6e5082fcddc8f548f83a14d4
Tadiwanashe Shangwa <tshxng@gmail.com>
2022-12-10 22:12:19 +0200

    fet:changed button colors

---
+++ src/components/LinkButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../pages/App.css"
+
+export default function ({}) {
+  return (
+    <div>
+        <a className='link_button' href='http://instagram.com/tadiwadev' > Instagram</a>
+    </div>
+  )
+}
